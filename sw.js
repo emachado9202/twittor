11ff2bb8f99e0563229251af160f68f086df9101
emachado9202 <emachado9202@gmail.com>
2021-05-16 08:09:56 -0400

    try again

--- sw.js
+++ sw.js
@@ -30,12 +30,13 @@ self.addEventListener("install", (e) => {
   const cacheStatic = caches
     .open(STATIC_CACHE)
     .then((cache) => cache.addAll(APP_SHELL));
-
+  /*
   const cacheInmutable = caches
     .open(INMUTABLE_CACHE)
     .then((cache) => cache.addAll(APP_SHELL_INMUTABLE));
 
-  e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+  e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));*/
+  e.waitUntil(cacheStatic);
 });
 
 self.addEventListener("activate", (e) => {
